9d37121fe3bb312f1b4b8c50221bdad69b5b6607
MysterySuperhero <nazarkovdmitri10@yandex.ru>
2015-09-25 16:44:24 +0300

    modified admin page bolee-menee

--- public_html/js/views/admin.js
+++ public_html/js/views/admin.js
@@ -16,6 +16,40 @@ define([
 
         render: function() {
             $('#page').html(adminTmpl());
+            $.ajax({
+                    type: "GET",
+                    url: "/admin",
+                    data: null,
+                    success: function(data){
+                        var data = JSON.stringify(data);
+                        data = JSON.parse(data);
+                        var sessions = data['sessions'];
+                        for (i = 0; i < sessions.length; i++){
+                            $("#loggedTable").append("<tr><td>" + sessions[i] + "</td></tr>")
+                        }
+                        var allUsers = data['users'];
+                        for (i = 0; i < allUsers.length; i++){
+                            $("#usersTable").append("<tr><td>" + allUsers[i] + "</td></tr>")
+                        }
+                    },
+                    dataType: "json",
+
+            //         // statusCode: {
+            //         //     404: function() {
+            //         //         alert( "page not found" );
+            //         //  }
+
+
+            //         // .ajaxError() {
+            //         //     alert("405=(");
+            //         // }
+            //         // 405: function(){
+            //         //     alert("405=(");
+            //         // }
+                });
+            // $.get("/admin", function(data, status, xhr){
+            //     alert(JSON.stringify(data));
+            // }, "json");
         },
 
         show: function() {
