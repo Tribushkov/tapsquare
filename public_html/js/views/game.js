c19bea74c3c24acab65f14e2a29750028b5f4c1b
MysterySuperhero <nazarkovdmitri10@yandex.ru>
2015-10-25 17:53:59 +0300

    diman v turme

--- public_html/js/views/game.js
+++ public_html/js/views/game.js
@@ -13,45 +13,68 @@ define([
         initialize: function () {
             // TODO
         },
-        render: function () {
-            this.$el.html(gameTmpl());
+        
+        events: {
 
-            $(".game-cell").click(function() {
-                if ($(this).attr('class') == 'game-cell')
-                    $(this).addClass('hover');
-                else
-                    $(this).removeClass('hover');
-            });
+            'click .game_cell': 'burp'
+        
+        },
+
+        burp: function(){
+
+        },
+
+        render: function () {
+            this.$el.html(gameTmpl());   
+            for (var i = 0; i < 10; i++) {
+                for (var j = 0; j < 10; j++) {
+                    $('.main__gamescene').append('<div class="game-cell" id=\"' + i + '_' + j +'\"></div>');
+                }
+            }         
+
         },
         show: function () {
             this.$el.show();
 
-            // $.ajax({
-            //         type: "POST",
-            //         url: "/gamewaiting",
-            //         data: null,
-            //         success: function(){
-            //            alert("ZAPROS OTPRAVIL I POLUCHIL OTVET 200 OK")
-            //         },
-            // });
+            $.ajax({
+                    type: "POST",
+                    url: "/game",
+                    data: null,
+                    success: function(){
+                       // alert("ZAPROS OTPRAVIL I POLUCHIL OTVET 200 OK")
+                    },
+            }); 
 
             var socket = new WebSocket("ws://localhost:1488/game");
-              // отправить сообщение из формы publish
 
-              // обработчик входящих сообщений
-              socket.onopen = function(e){
-                alert("СОЕДИНЕНИЕ УСТАНОВЛЕНО");
-              }
-              socket.send("1 4 2");
-              socket.onmessage = function(event) {
+            socket.onopen = function(e){
+                // alert("СОЕДИНЕНИЕ");
+            };
+
+            socket.onmessage = function(event) {
                 var incomingMessage = event.data;
                 alert(incomingMessage);
-              };
+                var obj = JSON && JSON.parse(incomingMessage) || $.parseJSON(incomingMessage);
+                // alert("#" + obj.square);
+                $("#" + obj.square).addClass('hover');
+            };
+
+            $(".game-cell").click(function() {
+                // alert( $(this).attr('id') );
+                socket.send($(this).attr('id'));
+            });
+
+            $(".game-cell hover").click(function() {
+                // alert( $(this).attr('id') );
+                socket.send($(this).attr('id'));
+            });
 
         },
+
+
         hide: function () {
             this.$el.hide();
-        }
+        },
 
     });
 
